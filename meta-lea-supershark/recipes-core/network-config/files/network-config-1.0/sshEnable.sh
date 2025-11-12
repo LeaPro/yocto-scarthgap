4427344d7c7fe5e073fdd92eab45d0e4d5e70389
Dave <drbutler@leaprofessional.com>
2025-11-12 14:35:30 -0500

    block TCP port 1236 for external clients SuperShark

--- meta-lea-supershark/recipes-core/network-config/files/network-config-1.0/sshEnable.sh
+++ meta-lea-supershark/recipes-core/network-config/files/network-config-1.0/sshEnable.sh
@@ -1,4 +1,9 @@
 sed -i 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
 passwd --delete root
+# remove any existing iptables rules
 iptables --flush
-cp /dev/null /etc/iptables/iptables.rules
+# allow LEA API access on port 1236 for loopback interface (internal clients)
+iptables -A INPUT -i lo -p tcp --dport 1236 -j ACCEPT
+# block LEA API access on port 1236 from other interfaces (external clients)
+iptables -A INPUT -p tcp --dport 1236 -j DROP
+iptables-save > /etc/iptables/iptables.rules
